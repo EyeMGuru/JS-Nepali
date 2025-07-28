af4dc57a81dd4962a8246439752b39e64e7ff32d
EyeMGuru <120453405+EyeMGuru@users.noreply.github.com>
2025-07-28 11:05:17 +0000

    JS operation file added

---
+++ 01-Basics/04-operations.js
@@ -0,0 +1,17 @@
+// console.log((24+15)*12/6)
+
+// console.log(2>1)
+
+// console.log("2c">"b")
+
+// console.log("b">"a")
+
+// console.log("b">true)
+
+// console.log(Number(undefined))
+
+// console.log(undefined==1)
+
+// console.log(undefined>null)
+
+console.log(undefined == 0)
